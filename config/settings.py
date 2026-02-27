8b70dd4e7a0a647589d22d1d6fafe99dac0c1005
Harsh Mishra <harshx@zohomail.in>
2026-02-27 09:07:18 +0530

    making the project ready for dev

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ DATABASE_URL = config("DATABASE_URL")
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", default="dev-secret-key")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
